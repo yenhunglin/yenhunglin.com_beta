387e0f16bebf423cd534e97a4ee15895a39e6ec7
Yen Hung Lin <lllyyyhhh45@hotmail.com>
2020-05-25 15:38:02 -0700

    updates index_2

---
+++ build/precache-manifest.3cfb4f560a451af1d02acfc9554ccb4f.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6040d91bd598f2349723989e6bea64a1",
+    "url": "/yenhunglin.com_beta/index.html"
+  },
+  {
+    "revision": "ab2c74f95e1c347360d1",
+    "url": "/yenhunglin.com_beta/static/css/main.9f20f834.chunk.css"
+  },
+  {
+    "revision": "0f15172aaae1f45ee8f7",
+    "url": "/yenhunglin.com_beta/static/js/2.d6943451.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yenhunglin.com_beta/static/js/2.d6943451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab2c74f95e1c347360d1",
+    "url": "/yenhunglin.com_beta/static/js/main.8cb572c6.chunk.js"
+  },
+  {
+    "revision": "7379f3a84cad33b8905f",
+    "url": "/yenhunglin.com_beta/static/js/runtime-main.e976cd58.js"
+  },
+  {
+    "revision": "16af57cb204a3e4fc0ad7c884b0bafdf",
+    "url": "/yenhunglin.com_beta/static/media/1.16af57cb.png"
+  },
+  {
+    "revision": "83bfddfb2d05967dc55efa59cb5800c0",
+    "url": "/yenhunglin.com_beta/static/media/10.83bfddfb.png"
+  },
+  {
+    "revision": "9813ed0a469e8e09f21502605c765df6",
+    "url": "/yenhunglin.com_beta/static/media/11.9813ed0a.png"
+  },
+  {
+    "revision": "5804fe33a28ae471efa909935d07512c",
+    "url": "/yenhunglin.com_beta/static/media/12.5804fe33.png"
+  },
+  {
+    "revision": "e954e603dce8dfbe3fa1ff12049a94c8",
+    "url": "/yenhunglin.com_beta/static/media/13.e954e603.png"
+  },
+  {
+    "revision": "ed8abf3e32a8e8d2953ad93d282d1283",
+    "url": "/yenhunglin.com_beta/static/media/2.ed8abf3e.png"
+  },
+  {
+    "revision": "5ac74c04fc6c77093ec1fe73065bf41b",
+    "url": "/yenhunglin.com_beta/static/media/3.5ac74c04.png"
+  },
+  {
+    "revision": "c7a09b7d1457a2c0143b574aa7eee5c7",
+    "url": "/yenhunglin.com_beta/static/media/4.c7a09b7d.png"
+  },
+  {
+    "revision": "297587f5bae214efb17a6f7a13ad8626",
+    "url": "/yenhunglin.com_beta/static/media/5.297587f5.png"
+  },
+  {
+    "revision": "342fc089cfc73b7bf9e37f2a74df2ac5",
+    "url": "/yenhunglin.com_beta/static/media/6.342fc089.png"
+  },
+  {
+    "revision": "381a98f2269a53f35b947a0c73cff80a",
+    "url": "/yenhunglin.com_beta/static/media/7.381a98f2.png"
+  },
+  {
+    "revision": "0a69aec0b803bb54b295fc4f8a74f773",
+    "url": "/yenhunglin.com_beta/static/media/8.0a69aec0.png"
+  },
+  {
+    "revision": "39fe8e107d8ca9d65ab4831d09838b9b",
+    "url": "/yenhunglin.com_beta/static/media/9.39fe8e10.png"
+  },
+  {
+    "revision": "932e9673990b59694818a462653e0019",
+    "url": "/yenhunglin.com_beta/static/media/behance.932e9673.png"
+  },
+  {
+    "revision": "b1e520d7e39ba36db1ad3ca18c25fe49",
+    "url": "/yenhunglin.com_beta/static/media/dribbble.b1e520d7.png"
+  },
+  {
+    "revision": "3823c284e139bd2af0c8c8e5010a1511",
+    "url": "/yenhunglin.com_beta/static/media/header-2.3823c284.png"
+  },
+  {
+    "revision": "35db437d6ee6addfb81ac7004ea9ee0f",
+    "url": "/yenhunglin.com_beta/static/media/header.35db437d.png"
+  },
+  {
+    "revision": "b5a94f905a30911c61f8a29374e7661a",
+    "url": "/yenhunglin.com_beta/static/media/ig.b5a94f90.png"
+  },
+  {
+    "revision": "d4bd4239c04b93a1b6a46add9e1f2849",
+    "url": "/yenhunglin.com_beta/static/media/medium.d4bd4239.png"
+  },
+  {
+    "revision": "145f9d7b63c168fbdc788d92aedf6418",
+    "url": "/yenhunglin.com_beta/static/media/pinterest.145f9d7b.png"
+  }
+]);
